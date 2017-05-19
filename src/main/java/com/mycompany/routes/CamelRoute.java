1a166cf935ff7fba38adc2078029fc77f25700fd
Renato Campelo <renato.campelo@ati.pe.gov.br>
2017-05-19 11:06:30 -0300

    using camel-jdbc works

--- src/main/java/com/mycompany/routes/CamelRoute.java
+++ src/main/java/com/mycompany/routes/CamelRoute.java
@@ -1,17 +1,25 @@
 package com.mycompany.routes;
 
 
-import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.processor.idempotent.jpa.JpaMessageIdRepository;
+import javax.sql.DataSource;
 
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.processor.idempotent.jdbc.JdbcMessageIdRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class CamelRoute extends RouteBuilder {
 
+	@Autowired private DataSource dataSource;
+	
 	@Override
 	public void configure() throws Exception {
+		
 		// TODO Auto-generated method stub
-		from("jpa:com.mycompany.model.Client?persistenceUnit=persistenceUnit&consumeDelete=false")
+		from("jpa:com.mycompany.model.Client?persistenceUnit=persistenceUnit&consumeDelete=false&consumer.delay=10000")
 		.split().simple("${body}")
-			.idempotentConsumer(simple("${body.id}/${body.name}"), JpaMessageIdRepository.jpaMessageIdRepository("persistenceUnit", "ClientRepository"))
+			.idempotentConsumer(simple("${body.id}/${body.name}"), new JdbcMessageIdRepository(dataSource, "ClientConsumer"))
 			.log("${body.name} processed");
 	}
 
